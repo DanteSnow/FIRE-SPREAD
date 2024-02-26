018ae329b81a89a23682643ab1a14233a48ea69d
clyde <clydehan0401@gmail.com>
2024-02-26 15:11:52 +0900

    feat: 투두 완료 기능 구현

--- src/components/CompleteButton.tsx
+++
@@ -1,3 +0,0 @@
-export default function CompleteButton() {
-  return <button>완료</button>;
-}
--- src/components/TodayTodo.tsx
+++ src/components/TodayTodo.tsx
@@ -1,11 +1,30 @@
-import CompleteButton from "./CompleteButton";
+import { doc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 import { ITodo } from "./TodayTodoList";
 
-export default function TodayTodo({ todo, complete }: ITodo) {
+export default function TodayTodo({ todo, complete, userId, id }: ITodo) {
+  const user = auth.currentUser;
+
+  const onCompleted = async () => {
+    if (user?.uid !== userId) return;
+    try {
+      await updateDoc(doc(db, "todo", id), {
+        complete: true,
+        completedAt: Date.now(),
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      //
+    }
+  };
+
   if (!complete) {
     return (
       <>
-        <CompleteButton />
+        {user?.uid === userId ? (
+          <button onClick={onCompleted}>완료</button>
+        ) : null}
         <div>{todo}</div>
       </>
     );
