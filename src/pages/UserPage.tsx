1745e3fcce0a6769e3dd85e96e5bc6f8ca2caa53
clyde <clydehan0401@gmail.com>
2024-02-27 20:08:56 +0900

    feat: 프로필 클릭 시 로그인 정보와 일치하면 마이페이지 이동 로직 구현

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,6 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { auth } from "../firebase";
+import { useEffect } from "react";
 
 export default function UserPage() {
   const { userId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        if (user.uid === userId) {
+          navigate("/mypage");
+        }
+      }
+    });
+    return () => {
+      unsubscribe && unsubscribe();
+    };
+  });
+
   return <h1>{userId} 님의 페이지입니다</h1>;
 }
