44a7c92f5f7b51479e9b4de77fdb2166fc28993e
clyde <clydehan0401@gmail.com>
2024-03-01 22:51:36 +0900

    feat: 전체 css 기본 틀 구현 완료

--- src/components/GuestBook.tsx
+++ src/components/GuestBook.tsx
@@ -5,9 +5,9 @@ export default function GuestBook({ guestBook, username }: IGuestBook) {
     <>
       <div className="mb-3">
         <span>이미지</span>
-        <span className="mt-5 pb-2 pl-5 text-lg font-bold">{username}</span>
+        <span className="pb-2 pl-5 text-lg font-bold">{username}</span>
       </div>
-      <div className="mb-3 h-64 rounded-xl border-2 border-white p-10 ">
+      <div className="mb-12 h-64 rounded-xl border-2 border-white p-10 ">
         <span>{guestBook}</span>
       </div>
     </>
