168966ea26de90697501cddfe5915d1299b9e964
clyde <clydehan0401@gmail.com>
2024-03-04 22:25:21 +0900

    fix: PageHeader 크기 조정

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -5,8 +5,8 @@ export default function PageHeader() {
   const userName = useRecoilValue(userNameState);
 
   return (
-    <header className="rounded-tr-3xl bg-gray-500 py-16 pl-10">
-      <h1 className="text-8xl font-bold text-gray-700">{userName}</h1>
+    <header className="rounded-tr-3xl bg-gray-500 py-4 pl-10">
+      <h1 className="text-2xl font-bold text-gray-700">{userName}</h1>
     </header>
   );
 }
