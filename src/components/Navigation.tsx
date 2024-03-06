bf6800517a060483beb78b4907123aeb19e8af19
clyde <clydehan0401@gmail.com>
2024-03-06 20:24:39 +0900

    feat: 왼쪽 메뉴 호버 시 색상 변경

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -18,20 +18,20 @@ export default function Navigation() {
   return (
     <nav className="flex flex-grow flex-col justify-center gap-2">
       <Link to="/homepage">
-        <div className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1">
+        <div className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1 hover:bg-gray-600">
           <img className="h-7" src={homeIcon} />
           <span className="text-sm font-bold">HOME</span>
         </div>
       </Link>
       <Link to="/mypage">
-        <div className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1">
+        <div className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1 hover:bg-gray-600">
           <img className="h-7" src={userIcon} />
           <span className="text-sm font-bold ">MY PAGE</span>
         </div>
       </Link>
       <div
         onClick={onSignOut}
-        className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1"
+        className="h-13 flex w-full cursor-pointer items-center gap-3 rounded-2xl bg-gray-700 p-3 text-white transition-transform duration-300 hover:-translate-x-1 hover:-translate-y-1 hover:bg-gray-600"
       >
         <img className="h-7" src={logoutIcon} />
         <span className="text-sm font-bold ">LOG OUT</span>
