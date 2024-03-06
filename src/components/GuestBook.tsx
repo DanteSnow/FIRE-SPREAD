98ac2ddaa1be571c534ee93eaf0fe622c377d763
clyde <clydehan0401@gmail.com>
2024-03-06 13:30:29 +0900

    feat: 방명록 프로필 클릭 시 해당 유저페이지 이동

--- src/components/GuestBook.tsx
+++ src/components/GuestBook.tsx
@@ -4,6 +4,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import { UserProfile } from "./HomeTodayTodoList";
 import defaultIcon from "../images/user.svg";
+import { Link } from "react-router-dom";
 
 export default function GuestBook({ guestBook, username, userId }: IGuestBook) {
   const [userProfiles, setUserProfiles] = useState<UserProfile>({});
@@ -22,14 +23,16 @@ export default function GuestBook({ guestBook, username, userId }: IGuestBook) {
 
   return (
     <>
-      <div className="mb-4 flex items-center gap-3">
-        <img
-          src={userProfiles[userId] ? userProfiles[userId] : defaultIcon}
-          alt={userId}
-          className="h-9 w-9 overflow-hidden rounded-full"
-        />
-        <span className="text-sm">{username}</span>
-      </div>
+      <Link to={`/userpage/${userId}`}>
+        <div className="mb-4 flex items-center gap-3">
+          <img
+            src={userProfiles[userId] ? userProfiles[userId] : defaultIcon}
+            alt={userId}
+            className="h-9 w-9 overflow-hidden rounded-full"
+          />
+          <span className="text-sm">{username}</span>
+        </div>
+      </Link>
       <div className="mb-8 whitespace-pre-wrap break-all rounded-3xl border-none bg-gray-700 p-10 scrollbar-hide">
         <div>{guestBook}</div>
       </div>
