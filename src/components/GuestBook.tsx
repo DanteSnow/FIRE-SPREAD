93b96588267715dfba200cda43f2185343ebd9f0
clyde <clydehan0401@gmail.com>
2024-03-06 14:52:25 +0900

    feat: 방명록 생성 날짜 렌더링

--- src/components/GuestBook.tsx
+++ src/components/GuestBook.tsx
@@ -6,8 +6,24 @@ import { UserProfile } from "./HomeTodayTodoList";
 import defaultIcon from "../images/user.svg";
 import { Link } from "react-router-dom";
 
-export default function GuestBook({ guestBook, username, userId }: IGuestBook) {
+export default function GuestBook({
+  guestBook,
+  createdAt,
+  username,
+  userId,
+}: IGuestBook) {
   const [userProfiles, setUserProfiles] = useState<UserProfile>({});
+  const createdAtDate = new Date(createdAt);
+  const createdAtString =
+    createdAtDate.getFullYear() +
+    "." +
+    (createdAtDate.getMonth() + 1) +
+    "." +
+    createdAtDate.getDate() +
+    " " +
+    createdAtDate.getHours() +
+    ":" +
+    ("0" + createdAtDate.getMinutes()).slice(-2);
 
   useEffect(() => {
     const fetchUserProfiles = async () => {
@@ -24,7 +40,7 @@ export default function GuestBook({ guestBook, username, userId }: IGuestBook) {
   return (
     <>
       <Link to={`/userpage/${userId}`}>
-        <div className="mb-4 flex items-center gap-3">
+        <div className="flex items-center gap-3">
           <img
             src={userProfiles[userId] ? userProfiles[userId] : defaultIcon}
             alt={userId}
@@ -33,7 +49,10 @@ export default function GuestBook({ guestBook, username, userId }: IGuestBook) {
           <span className="text-sm">{username}</span>
         </div>
       </Link>
-      <div className="mb-8 whitespace-pre-wrap break-all rounded-3xl border-none bg-gray-700 p-10 scrollbar-hide">
+      <span className="mb-2 mr-2 text-right text-xs text-gray-400">
+        {createdAtString}
+      </span>
+      <div className="mb-8 whitespace-pre-wrap break-all rounded-2xl border-none bg-gray-700 p-10 scrollbar-hide">
         <div>{guestBook}</div>
       </div>
     </>
